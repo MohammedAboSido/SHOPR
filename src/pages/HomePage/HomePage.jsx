14eeba63151e6b71314c45bfe3c66ba93af91153
Mohammed Abo Sido <abosido2@gmail.com>
2023-09-27 16:51:49 +0300

    last

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,3 +1,4 @@
+
 import img from "../../assets/images/Dot.svg";
 import img1 from "../../assets/images/log01.svg";
 import img2 from "../../assets/images/log02.svg";
@@ -15,21 +16,21 @@ import Offer from "../../components/offer section/Offer";
 import Footer from "./../../components/Footer/Footer";
 import { HomePageWrapper } from "./style";
 const HomePage = () => {
-  return (
-    <HomePageWrapper>
-      <Offer />
-      <Navbar img1={img1} img2={img2} />
-      <Hero />
-      <Categories />
-      <Collection />
-      <Additions img={img} />
-      <Brands />
-      <Trending />
+    return (
+        <HomePageWrapper>
+            <Offer/>
+            <Navbar img1={img1} img2={img2} />
+            <Hero/>
+            <Categories />
+            <Collection />
+            <Additions img={img} />
+            <Brands />
+            <Trending />
       <Newsletter />
-      <Instagram />
+            <Instagram />
       <Footer />
-    </HomePageWrapper>
-  );
-};
+        </HomePageWrapper>
+    );
+}
 
 export default HomePage;
