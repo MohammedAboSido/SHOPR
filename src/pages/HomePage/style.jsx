b5bd86190513f95784959950a3c78d65655ce091
Mohammed Abo Sido <abosido2@gmail.com>
2023-09-27 16:53:20 +0300

    last

--- src/pages/HomePage/style.jsx
+++ src/pages/HomePage/style.jsx
@@ -1,5 +1,5 @@
 import styled from "@emotion/styled";
 
-export const HomePageWrapper=styled('div')`
-label:HomePageWrapper;
-`
+export const HomePageWrapper = styled("div")`
+  label: HomePageWrapper;
+`;
