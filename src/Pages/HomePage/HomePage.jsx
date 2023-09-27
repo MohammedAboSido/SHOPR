606736a9c3042b52ab08d2d7069757de872eab4d
Mohammed Abo Sido <abosido2@gmail.com>
2023-09-27 10:39:39 +0300

    fix

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,4 +1,3 @@
-
 import img from "../../assets/images/Dot.svg";
 import img1 from "../../assets/images/log01.svg";
 import img2 from "../../assets/images/log02.svg";
@@ -8,7 +7,6 @@ import Categories from "../../components/Categories";
 import Collection from "../../components/Collection";
 import Navbar from "../../components/Navbar/Navbar";
 import Newsletter from "../../components/Newsletter/Newsletter";
-
 import Trending from "../../components/Trending/Trending";
 import Hero from "../../components/hero section/hero";
 import Instagram from "../../components/instagram section/instagram";
@@ -16,21 +14,21 @@ import Offer from "../../components/offer section/Offer";
 import Footer from "./../../components/Footer/Footer";
 import { HomePageWrapper } from "./style";
 const HomePage = () => {
-    return (
-        <HomePageWrapper>
-            <Offer/>
-            <Navbar img1={img1} img2={img2} />
-            <Hero/>
-            <Categories />
-            <Collection />
-            <Additions img={img} />
-            <Brands />
-            <Trending />
+  return (
+    <HomePageWrapper>
+      <Offer />
+      <Navbar img1={img1} img2={img2} />
+      <Hero />
+      <Categories />
+      <Collection />
+      <Additions img={img} />
+      <Brands />
+      <Trending />
       <Newsletter />
-            <Instagram />
+      <Instagram />
       <Footer />
-        </HomePageWrapper>
-    );
-}
+    </HomePageWrapper>
+  );
+};
 
 export default HomePage;
