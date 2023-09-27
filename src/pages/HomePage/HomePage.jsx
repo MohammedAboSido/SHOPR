a1b0f09261c90d2dfaba685e9da6734b0874f0fb
Abdullah2000Habib <abd3habib@gmail.com>
2023-09-27 16:44:27 +0300

    LATEST CHANGES

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,3 @@
-
 import img from "../../assets/images/Dot.svg";
 import img1 from "../../assets/images/log01.svg";
 import img2 from "../../assets/images/log02.svg";
@@ -7,14 +6,12 @@ import Brands from "../../components/Brands/Brands";
 import Categories from "../../components/Categories";
 import Collection from "../../components/Collection";
 import Navbar from "../../components/Navbar/Navbar";
-<<<<<<< HEAD
-=======
 import Newsletter from "../../components/Newsletter/Newsletter";
->>>>>>> b01fca40eaf94dcf7e9040a7601f99255eabc5e2
 import Trending from "../../components/Trending/Trending";
 import Hero from "../../components/hero section/hero";
 import Instagram from "../../components/instagram section/instagram";
 import Offer from "../../components/offer section/Offer";
+import Footer from "./../../components/Footer/Footer";
 import { HomePageWrapper } from "./style";
 const HomePage = () => {
   return (
