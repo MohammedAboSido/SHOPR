6445691275b19424c5235c3b799f42ae2f0d51fa
Abdullah2000Habib <abd3habib@gmail.com>
2023-09-27 12:04:38 +0300

    Left Component

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,3 +1,4 @@
+
 import img from "../../assets/images/Dot.svg";
 import img1 from "../../assets/images/log01.svg";
 import img2 from "../../assets/images/log02.svg";
@@ -6,29 +7,25 @@ import Brands from "../../components/Brands/Brands";
 import Categories from "../../components/Categories";
 import Collection from "../../components/Collection";
 import Navbar from "../../components/Navbar/Navbar";
-import Newsletter from "../../components/Newsletter/Newsletter";
 import Trending from "../../components/Trending/Trending";
 import Hero from "../../components/hero section/hero";
 import Instagram from "../../components/instagram section/instagram";
 import Offer from "../../components/offer section/Offer";
-import Footer from "./../../components/Footer/Footer";
 import { HomePageWrapper } from "./style";
 const HomePage = () => {
-  return (
-    <HomePageWrapper>
-      <Offer />
-      <Navbar img1={img1} img2={img2} />
-      <Hero />
-      <Categories />
-      <Collection />
-      <Additions img={img} />
-      <Brands />
-      <Trending />
-      <Newsletter />
-      <Instagram />
-      <Footer />
-    </HomePageWrapper>
-  );
-};
+    return (
+        <HomePageWrapper>
+            <Offer/>
+            <Navbar img1={img1} img2={img2} />
+            <Hero/>
+            <Categories />
+            <Collection />
+            <Additions img={img} />
+            <Brands />
+            <Trending />
+            <Instagram />
+        </HomePageWrapper>
+    );
+}
 
 export default HomePage;
