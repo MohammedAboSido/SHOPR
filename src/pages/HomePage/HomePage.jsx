af6530599f38d86ecde58ba09fa9ac579db34589
Abdullah2000Habib <abd3habib@gmail.com>
2023-10-01 11:03:49 +0300

    merge from Abdullah

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,29 +1,32 @@
 import img from "../../assets/images/Dot.svg";
-import Layout from "../../components/Layout";
-import {
-  Additions,
-  Brands,
-  Categories,
-  Collection,
-  Hero,
-  Instagram,
-  Newsletter,
-  Trending,
-} from "./index";
+import img1 from "../../assets/images/log01.svg";
+import img2 from "../../assets/images/log02.svg";
+import Additions from "../../components/Additions";
+import Brands from "../../components/Brands/Brands";
+import Categories from "../../components/Categories";
+import Collection from "../../components/Collection";
+import Navbar from "../../components/Navbar/Navbar";
+import Newsletter from "../../components/Newsletter/Newsletter";
+import Trending from "../../components/Trending/Trending";
+import Hero from "../../components/hero section/hero";
+import Instagram from "../../components/instagram section/instagram";
+import Offer from "../../components/offer section/Offer";
+import Footer from "./../../components/Footer/Footer";
 import { HomePageWrapper } from "./style";
 const HomePage = () => {
   return (
     <HomePageWrapper>
-      <Layout>
-        <Hero />
-        <Categories />
-        <Collection />
-        <Additions img={img} />
-        <Brands />
-        <Trending />
-        <Newsletter />
-        <Instagram />
-      </Layout>
+      <Offer />
+      <Navbar img1={img1} img2={img2} />
+      <Hero />
+      <Categories />
+      <Collection />
+      <Additions img={img} />
+      <Brands />
+      <Trending />
+      <Newsletter />
+      <Instagram />
+      <Footer />
     </HomePageWrapper>
   );
 };
